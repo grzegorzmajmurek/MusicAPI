bc200a53e684dfb13d5fcda0328d7613c2eb6cee
grzegorzmajmurek <61845216+grzegorzmajmurek@users.noreply.github.com>
2021-11-25 12:51:53 +0100

    add error handling

--- MusicAPI/MusicAPI/Controllers/SongsController.cs
+++ MusicAPI/MusicAPI/Controllers/SongsController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MusicAPI.Data;
 using MusicAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MusicAPI.Controllers
 {
@@ -12,34 +15,62 @@ namespace MusicAPI.Controllers
     [ApiController]
     public class SongsController : ControllerBase
     {
-        private static List<Song> songs = new List<Song>()
-        {
-            new Song(){ Id = 0, Title = "Bo jestes Ty", Language = "Polish" },
-            new Song(){ Id = 1, Title = "Chcialem Byc", Language = "Polish" }
-        };
+        private ApiDbContext _dbContext;
 
+        public SongsController(ApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET: api/<SongsController>
         [HttpGet]
-        public IEnumerable<Song> Get()
+        public IActionResult Get()
         {
-            return songs;
+            return Ok(_dbContext.Songs);
         }
 
+        // GET api/<SongsController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var song = _dbContext.Songs.Find(id);
+            return Ok(song);
+        }
+
+        // POST api/<SongsController>
         [HttpPost]
-        public void Post([FromBody]Song song)
+        public IActionResult Post([FromBody] Song song)
         {
-            songs.Add(song);
+            _dbContext.Songs.Add(song);
+            _dbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
         }
 
+        // PUT api/<SongsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]Song song)
+        public IActionResult Put(int id, [FromBody] Song songObj)
         {
-            songs[id] = song;
+            var song = _dbContext.Songs.Find(id);
+            if (song == null)
+            {
+                return NotFound("No record found against this Id");
+            }
+            else
+            {
+                song.Title = songObj.Title;
+                song.Language = songObj.Language;
+                _dbContext.SaveChanges();
+                return Ok("Record updated sucessfully");
+            }
         }
 
+        // DELETE api/<SongsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
-            songs.RemoveAt(id);
+            var song = _dbContext.Songs.Find(id);
+            _dbContext.Songs.Remove(song);
+            _dbContext.SaveChanges();
+            return Ok("Record deleted");
         }
     }
 }
